ff531974decb657863c46f0648a51c6cb920ad5b
Joeyrmuzzo <57329326+Joeyrmuzzo@users.noreply.github.com>
2020-03-31 14:26:57 -0400

    Add files via upload

---
+++ week 6 quiz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_6_quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int value;
+            double[] numbers = new double[5];
+            for (int index = 0; index < 5; index++) 
+            {
+                Console.WriteLine("Please enter a number: ");
+                numbers[index] = Convert.ToDouble(Console.ReadLine());
+            }
+            {
+                Console.WriteLine("Please enter a number to subtract by: ");
+                value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("After subtracting by : " + value + " the numbers are :");
+            }
+            
+            for(int index = 0; index <5;index++)
+                {
+                numbers[index] = numbers[index] - value;
+                Console.WriteLine(numbers[index]);
+                }
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+            
+        }
+    }
+}
